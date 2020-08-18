21846039a05ae938f628eabe36c129d61bedbdd4
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-08-18 15:50:42 +0200

    insertion tweeter bootstrap pour affichage des biens sur la homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
 
-/**
- * le chemin(path)home on l'appele dans le fichier base avec le bouton immos
-* @Route("", name="home")
- * @return Response
-*/
-    public function index():Response{
-
+    /**
+     * le chemin(path)home on l'appele dans le fichier base avec le bouton immos
+     * @Route("", name="home")
+     * @param PropertyRepository $repository
+     * @return Response
+     */
+    public function index(PropertyRepository $repository):Response{
+             $properties= $repository->findLatest();
             //retourne la page qui est dans template
-            return $this->render('pages/home.html.twig');
+            return $this->render('pages/home.html.twig',[
+                'properties' => $properties
+        ]);
 
 
 }
