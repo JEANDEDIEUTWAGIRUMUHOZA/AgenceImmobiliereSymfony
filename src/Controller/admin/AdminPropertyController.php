f9fba9d8efa1ce939cabb9d983ee3896c606f292
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-08-26 11:22:52 +0200

    création méthode new et traitement de formulaire pour editer un bien

--- src/Controller/admin/AdminPropertyController.php
+++ src/Controller/admin/AdminPropertyController.php
@@ -4,7 +4,9 @@ namespace App\Controller\admin;
 use App\Entity\Property;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,9 +16,11 @@ class AdminPropertyController extends AbstractController{
      * @var PropertyRepository
      */
     private $repository;
+    private $em;
 
-    public function __construct(PropertyRepository $repository){
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em){
          $this->repository = $repository;
+         $this->em = $em;
     }
 
     /**
@@ -30,12 +34,41 @@ class AdminPropertyController extends AbstractController{
     }
 
     /**
-     * @Route("/admin/{id}", name="admin.property.edit")
+     * @Route("/admin/property/create", name="admin.property.new")
+     * @param Request $request
+     */
+     public function new(Request $request){
+
+         $property = new Property();
+         $form = $this->createForm(PropertyType::class, $property);
+         $form->handleRequest($request);
+
+         if($form->isSubmitted() &&  $form->isValid()){
+             $this->em->persist($property);
+             $this->em->flush();
+             return $this->redirectToRoute('admin.property.index');
+
+         }
+         return $this->render('admin/property/new.html.twig',[
+             'property' => $property,
+             'form' => $form->createView()
+         ]);
+     }
+    /**
+     * @Route("/admin/property{id}", name="admin.property.edit")
      * @param Property $property
+     * @param Request $request
      * @return Response
      */
-    public function edit(Property $property){
+    public function edit(Property $property, Request $request){
         $form = $this->createForm(PropertyType::class, $property);//pour utiliser le formulaire qu'on vient de créer
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() &&  $form->isValid()){
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+
+        }
         return $this->render('admin/property/edit.html.twig',[
             'property' => $property,
             'form' => $form->createView()
